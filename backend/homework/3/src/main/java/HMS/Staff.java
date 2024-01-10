d88ba6849c088774ab440a337e178a05d830856c
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-01-10 13:15:05 +0530

    Part 1 Done

---
+++ backend/homework/3/src/main/java/HMS/Staff.java
@@ -0,0 +1,39 @@
+package HMS;
+public class Staff extends  User {
+    private long staffId;
+    private int yearsOfExperience;
+    private String description;
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public long getStaffId() {
+        return staffId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setStaffId(long staffId) {
+        this.staffId = staffId;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+}
