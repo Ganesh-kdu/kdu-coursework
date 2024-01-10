b4e1f1e7849cc2600207fb606b17e8da32810ead
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-01-10 16:28:17 +0530

    temporary

---
+++ backend/homework/3/src/main/java/HMS/Billing.java
@@ -0,0 +1,13 @@
+package HMS;
+
+public class Billing {
+    public static double[] computePaymentAmount(Patient patient, double amount) {
+        double[] payments = new double[2];
+
+        HealthInsurancePlan patientInsurancePlan = patient.getInsurancePlan();
+
+        // your logic
+
+        return payments;
+    }
+}
---
+++ backend/homework/3/src/main/java/HMS/HealthInsurancePlan.java
@@ -0,0 +1,13 @@
+package HMS;
+
+public class HealthInsurancePlan {
+    // Code for 'coverage' field goes here
+    // Don't worry about the below code and also the InsuranceBrand class
+    private InsuranceBrand offeredBy;
+    public InsuranceBrand getOfferedBy() {
+        return offeredBy;
+    }
+    public void setOfferedBy(InsuranceBrand offeredBy) {
+        this.offeredBy = offeredBy;
+    }
+}
