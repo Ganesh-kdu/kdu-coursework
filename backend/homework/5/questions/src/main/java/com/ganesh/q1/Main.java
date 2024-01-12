420701e70d6ed8aa6c15342b0d778eea7c64c372
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-01-12 16:36:26 +0530

    Created homework 5

---
+++ backend/homework/5/questions/src/main/java/com/ganesh/q1/Main.java
@@ -0,0 +1,67 @@
+package com.ganesh.q1;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    private static int target;
+    private static class FactorialAndFactors implements Runnable{
+        private AtomicInteger flag = new AtomicInteger(0);
+        public int factorial() {
+            int result = 1;
+            for (int i=1; i<target; i++){
+                result*=i;
+            }
+            System.out.println("Factorial done");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                return 0;
+            }
+            return result;
+        }
+        public ArrayList<Integer> factors() {
+            ArrayList<Integer> result = new ArrayList<Integer>();
+            for (int i = 1; i < target; i++) {
+                if (target % i == 0) {
+                    result.add(i);
+                }
+            }
+            System.out.println("Factors done");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                return new ArrayList<Integer>();
+            }
+            return result;
+        }
+        @Override
+        public void run() {
+            if(0==flag.getAndAdd(1)){
+                System.out.println(factorial());
+            }else {
+                System.out.println(factors().toString());
+            }
+            
+        }
+    }
+    public static void main(String[] args){
+        target = 10;
+        FactorialAndFactors calculate = new FactorialAndFactors();
+        Thread thread = new Thread(calculate);
+        Thread thread2= new Thread(calculate);
+        thread.start();
+        thread2.start();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
