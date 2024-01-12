38676a64a5006b5553d5f879afacb00bc2dca31c
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-01-12 10:14:05 +0530

    Update logger for future use

--- backend/homework/4/logger/src/main/java/com/ganesh/LogMaster.java
+++ backend/homework/4/logger/src/main/java/com/ganesh/LogMaster.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogMaster {
+    private LogMaster() {
+        throw new IllegalStateException("Utility class");
+    }
     private static final Logger slf4jLogger = LoggerFactory.getLogger(LogMaster.class);
     public static void print(int data){
         slf4jLogger.debug("{}",data);
