367e14f60dbee365b926f9f6e73fbd9f4ce7bf30
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-01-24 11:25:17 +0530

    Added files from hands-on 1 and 2

---
+++ backend/homework/8/src/main/java/org/example/services/SpeakerService.java
@@ -0,0 +1,18 @@
+package org.example.services;
+
+import org.example.entities.Speaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpeakerService {
+    @Bean(name = "sony")
+    Speaker speaker1(){
+        return new Speaker("Sony",1000);
+    }
+
+    @Bean(name = "bose")
+    Speaker speaker2(){
+        return new Speaker("Bose",10000);
+    }
+}
