0e88eceb842695f2fb8f907a4e24b5057b11c9ba
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-01-25 10:49:16 +0530

    Hands on 3 done

---
+++ backend/homework/8/src/main/java/com/example/sbhandson1/controller/VehicleController.java
@@ -0,0 +1,41 @@
+package com.example.sbhandson1.controller;
+
+import com.example.sbhandson1.DTO.VehicleRequestDto;
+import com.example.sbhandson1.DTO.VehicleResponseDto;
+import com.example.sbhandson1.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping("/create-vehicle")
+    public ResponseEntity<VehicleResponseDto> createVehicle(@RequestBody VehicleRequestDto vehicleDto) {
+        return new ResponseEntity<>(vehicleService.createVehicle(vehicleDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get-vehicle/{id}")
+    public ResponseEntity<VehicleResponseDto> getVehicle(@PathVariable Integer id) {
+        return new ResponseEntity<>(vehicleService.getVehicle(id),HttpStatus.OK);
+    }
+
+    @PutMapping("/update-vehicle/{id}")
+    public ResponseEntity<VehicleResponseDto> updateVehicle(@PathVariable Integer id, @RequestBody VehicleRequestDto vehicleRequestDto) {
+        return new ResponseEntity<>(vehicleService.updateVehicle(id, vehicleRequestDto),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete-vehicle/{id}")
+    public ResponseEntity<String> deleteVehicle(@PathVariable Integer id) {
+        return new ResponseEntity<>(vehicleService.deleteVehicle(id).getMessage(),HttpStatus.OK);
+    }
+
+    @GetMapping("/expensive-vehicles")
+    public ResponseEntity<VehicleResponseDto> getMostOrLeastExpensiveVehicle(@RequestParam(required = false) String sortDirection) {
+        return new ResponseEntity<>(vehicleService.getMostOrLeastExpensiveVehicle(sortDirection), HttpStatus.OK);
+    }
+}
---
+++ backend/homework/8/src/main/java/com/example/sbhandson1/repository/VehicleInventory.java
@@ -0,0 +1,48 @@
+package com.example.sbhandson1.repository;
+
+import com.example.sbhandson1.DTO.VehicleResponseDto;
+import com.example.sbhandson1.entity.Vehicle;
+import org.springframework.stereotype.Repository;
+
+import java.util.Comparator;
+import java.util.HashMap;
+@Repository
+public class VehicleInventory {
+    private HashMap<Integer, Vehicle> vehicleList;
+    public VehicleInventory() {
+        this.vehicleList = new HashMap<>();
+    }
+    public VehicleResponseDto createVehicle(Vehicle vehicle) {
+        vehicleList.put(vehicle.getId(), vehicle);
+        return new VehicleResponseDto(vehicle, "Vehicle created successfully");
+    }
+    public Vehicle getVehicle(int id) {
+        if (vehicleList.get(id)!=null){
+                return new Vehicle(vehicleList.get(id).getId(), vehicleList.get(id).getCompany(), vehicleList.get(id).getYear(), vehicleList.get(id).getColor(), vehicleList.get(id).getPrice());
+        }
+        return null;
+    }
+    public VehicleResponseDto updateVehicle(int id, Vehicle vehicle) {
+            if(vehicleList.computeIfPresent(id,(k,v)->vehicle) != null) {
+                return new VehicleResponseDto(vehicle, "Vehicle updated successfully");
+            }
+
+        return new VehicleResponseDto(null, "Vehicle not found");
+    }
+
+    public void deleteVehicle(int id) {
+            if(vehicleList.get(id) != null) {
+                vehicleList.remove(id);
+            }
+    }
+
+    public VehicleResponseDto getMostOrLeastExpensiveVehicle(String sortDirection) {
+        Vehicle vehicle = null;
+        if (sortDirection.equals("asc")) {
+            vehicle = vehicleList.values().stream().max(Comparator.comparingInt(Vehicle::getPrice)).get();
+        } else if (sortDirection.equals("desc")) {
+            vehicle = vehicleList.values().stream().min(Comparator.comparingInt(Vehicle::getPrice)).get();
+        }
+        return new VehicleResponseDto(vehicle, "Vehicle found");
+    }
+}
