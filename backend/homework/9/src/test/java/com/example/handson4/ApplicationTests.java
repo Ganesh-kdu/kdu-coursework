166f9d8bab805a167cb7c0e14d70861f8fb4e0aa
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-01-25 12:11:36 +0530

    init handson 4

---
+++ backend/homework/9/src/test/java/com/example/handson4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handson4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
