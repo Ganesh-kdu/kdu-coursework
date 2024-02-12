b899b51c7d0fb98796b4169dbfbbaa7f1391e0e1
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-02-12 06:34:22 +0530

    smells

--- frontend/assignments/1/homepage/script.js
+++ frontend/assignments/1/homepage/script.js
@@ -135,16 +135,14 @@ function createPost(){
             }else{
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML=`${value-1}`;
             }
-        }else{
-            if(currentTarget.getElementsByClassName("stat_value").text==null || currentTarget.getElementsByClassName("stat_value").text==""){
+        }else if(currentTarget.getElementsByClassName("stat_value").text==null || currentTarget.getElementsByClassName("stat_value").text==""){
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML='1';
                 console.log(currentTarget);
             }else{
                 let value = parseInt(currentTarget.getElementsByClassName("stat_value").item(0).innerHTML);
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML =`${value+1}`;
             }
-        }
-        currentTarget.classList.toggle('retweeted')
+        currentTarget.classList.toggle('retweeted');
     })
     commentDiv.addEventListener('click',(event)=>{
         let currentTarget = event.currentTarget;
@@ -156,16 +154,14 @@ function createPost(){
             }else{
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML=`${value-1}`;
             }
-        }else{
-            if(currentTarget.getElementsByClassName("stat_value").text==null || currentTarget.getElementsByClassName("stat_value").text==""){
+        }else if(currentTarget.getElementsByClassName("stat_value").text==null || currentTarget.getElementsByClassName("stat_value").text==""){
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML='1';
                 console.log(currentTarget);
             }else{
                 let value = parseInt(currentTarget.getElementsByClassName("stat_value").item(0).innerHTML);
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML =`${value+1}`;
             }
-        }
-        currentTarget.classList.toggle('commented')
+        currentTarget.classList.toggle('commented');
     })
     likeDiv.addEventListener('click',(event)=>{
         let currentTarget = event.currentTarget;
@@ -175,7 +171,7 @@ function createPost(){
                 <path
                   d="M16.697 5.5c-1.222-.06-2.679.51-3.89 2.16l-.805 1.09-.806-1.09C9.984 6.01 8.526 5.44 7.304 5.5c-1.243.07-2.349.78-2.91 1.91-.552 1.12-.633 2.78.479 4.82 1.074 1.97 3.257 4.27 7.129 6.61 3.87-2.34 6.052-4.64 7.126-6.61 1.111-2.04 1.03-3.7.477-4.82-.561-1.13-1.666-1.84-2.908-1.91zm4.187 7.69c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"
                 ></path>
-              </svg>`
+              </svg>`;
             console.log(value);
             if (value==1){
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML="";
@@ -185,8 +181,7 @@ function createPost(){
         }else{
             currentTarget.getElementsByClassName("stat_icon").item(0).innerHTML=`<svg viewBox="0 0 32 32">
             <path d="M20.884 13.19c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path>
-              </svg>`
-            // console.log(value);
+              </svg>`;
             if(currentTarget.getElementsByClassName("stat_value").text==null || currentTarget.getElementsByClassName("stat_value").text==""){
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML='1';
                 console.log(currentTarget);
@@ -195,6 +190,6 @@ function createPost(){
                 currentTarget.getElementsByClassName("stat_value").item(0).innerHTML =`${value+1}`;
             }
         }
-        currentTarget.classList.toggle('liked')
+        currentTarget.classList.toggle('liked');
     })
 }
