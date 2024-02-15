23f75d3d765aac280c87ca60c3525eab3d72584a
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-02-15 17:00:19 +0530

    hands on 7 complete

--- frontend/homework/7/script.js
+++ frontend/homework/7/script.js
@@ -1,19 +1,19 @@
-var socket = io("http://127.0.0.1:3000");
-var form = document.getElementById("form");
-var input = document.getElementById("input");
+const socket = io("http://127.0.0.1:3000");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     if (input.value.trim() !== "") {
         console.log(input.value)
         socket.emit("message", input.value);
+        newMessage(input.value, true);
         input.value = "";
     }
-    newMessage(input.value, sent=true);
   });
 
 socket.on("new-message", (payload) => {
-    console.log(`Message received by client: ${payload}`);
+    newMessage(payload)
 });
 socket.on("connected",(payload)=>{
     console.log(payload);
@@ -21,25 +21,30 @@ socket.on("connected",(payload)=>{
 function newMessage(message, sent=false){
     let messageContainer = document.createElement("div");
     messageContainer.classList.add("container");
-
+    
     let profilePicture = document.createElement("div");
-    if(sent){
-        messageContainer.classList.add("sent");
-        profilePicture.innerText = "Y";
-    }else{
-        messageContainer.classList.add("received");
-        profilePicture.innerText = "G"
-    }
-    profilePicture.classList.add("pfp")
 
     let messageBox = document.createElement("div");
     messageBox.innerText = message;
     messageBox.classList.add("message-box");
+    
+    if(sent){
+        messageBox.classList.add("sent");
+        profilePicture.innerText = "Y";
+    }else{
+        messageBox.classList.add("received");
+        profilePicture.innerText = "G"
+    }
+    profilePicture.classList.add("pfp")
+
 
     messageContainer.appendChild(profilePicture);
     messageContainer.appendChild(messageBox);
 
-    document.getElementById("chat").appendChild(messageContainer);
+    let chat = document.getElementById("chat");
+
+    chat.appendChild(messageContainer);
+    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
 
 
 }
