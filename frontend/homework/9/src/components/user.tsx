2f876a03a95952d5796627f179138d01960376ac
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-02-20 22:55:01 +0530

    Cleaned code

--- frontend/homework/9/src/components/user.tsx
+++ frontend/homework/9/src/components/user.tsx
@@ -1,11 +1,15 @@
-import "./user.scss"
-interface userProps{name:string,fullName:string,designation:string}
-export const User = ({name,fullName,designation}:userProps) => {
-    return(
-        <div className="user-details">
-            <div className="detail">{name}</div>
-            <div className="sub-detail">{fullName}</div>
-            <div className="detail">{designation}</div>
-        </div>
-    );
+import "./user.scss";
+interface UserProps {
+  name: string;
+  fullName: string;
+  designation: string;
+}
+export const User = ({ name, fullName, designation }: UserProps) => {
+  return (
+    <div className="user-details">
+      <div className="detail">{name}</div>
+      <div className="sub-detail">{fullName}</div>
+      <div className="detail">{designation}</div>
+    </div>
+  );
 };
