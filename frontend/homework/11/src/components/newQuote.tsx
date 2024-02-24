590cb2db1ff7959b7c26a2bf770abeb118c062dc
Ganesh-kdu <ganeshg.setty@kickdrumtech.com>
2024-02-24 23:47:59 +0530

    Completed bonus goal: added a custom spinner on clicking 'New Quote' button

--- frontend/homework/11/src/components/newQuote.tsx
+++ frontend/homework/11/src/components/newQuote.tsx
@@ -1,14 +1,29 @@
-import "./newQuote.scss"
-function NewQuote({addQuote}:Readonly<{addQuote: Function}>){
+import { useState } from "react";
+import "./newQuote.scss";
+
+function NewQuote({ addQuote }: Readonly<{ addQuote: Function }>) {
+    const [loading, setLoading] = useState<boolean>(false);
     const getData = async () => {
+        setLoading(true);
         const response = await fetch(
             "https://api.quotable.io/quotes/random?limit=1"
         );
         const result = await response.json();
-        await addQuote((prev:string[]) => [...prev,...result]);
+        await addQuote((prev: string[]) => [...prev, ...result]);
+        setLoading(false);
     };
     return (
-        <button onClick={getData} className="quote-button">NEW QUOTE</button>
-    )
+        <>
+            { loading ? <div className="spinner"/> : null}
+            <button
+                onClick={getData}
+                className="quote-button"
+                id="quote-button"
+                disabled={loading}
+            >
+                NEW QUOTE
+            </button>
+        </>
+    );
 }
 export default NewQuote;
